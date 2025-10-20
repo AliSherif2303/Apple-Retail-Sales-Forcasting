5d0b639a26faae46532ebb4fa6d101364dcaf0ad
AliSherif2303 <alisherifsalaheldin@gmail.com>
2025-10-20 19:09:49 +0300

    start

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-def merge_data(d):
-    merged = (
-        d["sales"]
-        .merge(d["products"], left_on="product_id", right_on="Product_ID", how="left")
-        .merge(d["stores"], left_on="store_id", right_on="Store_ID", how="left")
-        .merge(d["category"], left_on="Category_ID", right_on="category_id", how="left")
-        .merge(d["warranty"], on="sale_id", how="left")
+def merge_all(datasets: dict):
+    sales = datasets['sales']
+    products = datasets['products']
+    stores = datasets['stores']
+    category = datasets['category']
+    warranty = datasets['warranty']
+
+    df = (
+        sales
+        .merge(products, left_on='product_id', right_on='Product_ID', how='left')
+        .merge(stores, left_on='store_id', right_on='Store_ID', how='left')
+        .merge(category, left_on='Category_ID', right_on='category_id', how='left')
+        .merge(warranty, on='sale_id', how='left')
     )
-    return merged
+    return df
