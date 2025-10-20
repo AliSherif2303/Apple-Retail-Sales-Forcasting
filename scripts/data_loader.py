428a67a070c7c67b55581c2422105b0c02b4480d
AliSherif2303 <alisherifsalaheldin@gmail.com>
2025-10-20 19:09:49 +0300

    start

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,11 +1,17 @@
 import pandas as pd
+from pathlib import Path
 
-def load_data(base_url):
-    data = {
-        "products": pd.read_csv(base_url + "Products.csv"),
-        "sales": pd.read_csv(base_url + "Sales.csv"),
-        "warranty": pd.read_csv(base_url + "Warranty.csv"),
-        "stores": pd.read_csv(base_url + "Stores.csv"),
-        "category": pd.read_csv(base_url + "Category.csv")
+def load_data(base_path='../data/raw'):
+    base = Path(base_path)
+    category = pd.read_csv(base / 'Category.csv')
+    products = pd.read_csv(base / 'Products.csv')
+    sales = pd.read_csv(base / 'Sales.csv')
+    stores = pd.read_csv(base / 'Stores.csv')
+    warranty = pd.read_csv(base / 'Warranty.csv')
+    return {
+        'category': category,
+        'products': products,
+        'sales': sales,
+        'stores': stores,
+        'warranty': warranty
     }
-    return data
